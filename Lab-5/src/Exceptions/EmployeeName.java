26b65aa4644c69ae8b9fe863750c22df03b2810b
vishal756 <92839425+vishal756@users.noreply.github.com>
2021-10-21 16:46:38 +0530

    java

---
+++ Lab-5/src/Exceptions/EmployeeName.java
@@ -0,0 +1,22 @@
+package Exceptions;
+import java.util.Scanner;
+
+public class EmployeeName {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("First Name");
+		String firstName=sc.nextLine();
+		System.out.println("Last Name");
+		String lastName=sc.nextLine();
+		try {
+			if(firstName.isEmpty() || lastName.isEmpty()) {//checks whether first or last name is empty
+				throw new NullPointerException();
+			   }
+			else
+				System.out.println(firstName+" "+lastName);
+		}catch(NullPointerException e) {
+			System.out.println("First and Last name should not be empty");
+		}
+		
+	  }
+}
